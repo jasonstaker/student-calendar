160c6f973d624d6ddbbcd8b5a5d285ddcefbf18c
Jason Staker <jstaker@student.ubc.ca>
2025-02-09 14:35:03 -0800

    Added tests for Calendar

--- src/test/model/CalendarTest.java
+++ src/test/model/CalendarTest.java
@@ -1,19 +1,23 @@
 package model;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CalendarTest {
     
+    Calendar calendar;
+
     @BeforeEach
     void setup() {
-
+        calendar = new Calendar("test");
     }
 
     @Test
     void testCalendarConstructor() {
-        assertTrue(true);
+        assertEquals("test", calendar.getTitle());
+        assertEquals(12, calendar.getMonths().length);
+        assertEquals(0, calendar.getCurrentMonthIndex());
     }
 }
