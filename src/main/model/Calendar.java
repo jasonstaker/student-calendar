c588de519d09663a22830d0234bbc14b4e50a43c
Jason Staker <jstaker@student.ubc.ca>
2025-02-09 14:27:14 -0800

    Added specification for Calendar

--- src/main/model/Calendar.java
+++ src/main/model/Calendar.java
@@ -3,7 +3,7 @@ package model;
 // A Calendar with a title that stores a list of months and a current month
 public class Calendar {
 
-    // EFFECTS: initializes a Calendar with a given title
+    // EFFECTS: initializes a new Calendar with a given title
     public Calendar(String title) {
 
     }
