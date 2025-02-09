5f96b2d5e86541079b0f5f8c5625d704c21c6e10
Jason Staker <jstaker@student.ubc.ca>
2025-02-09 14:48:52 -0800

    Implemented Calendar

--- src/main/model/Calendar.java
+++ src/main/model/Calendar.java
@@ -3,9 +3,39 @@ package model;
 // A Calendar with a title that stores a list of months and a current month
 public class Calendar {
 
+    private String title;
+    private Month[] months;
+    private int currentMonthIndex;
+
     // EFFECTS: initializes a new Calendar with a given title
     public Calendar(String title) {
+        this.title = title;
+        currentMonthIndex = 0;
+        months = new Month[12];
 
+        for(int i = 0; i < 12; i++) {
+            months[i] = new Month(i);
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: increments the month index if it does not exceed 11, nothing otherwise
+    public void incrementMonthIndex() {
+        currentMonthIndex++;
+        
+        if(currentMonthIndex > 11) {
+            currentMonthIndex = 11;
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: decrements the month index if it does not go below 0, nothing otherwise
+    public void decrementMonthIndex() {
+        currentMonthIndex--;
+
+        if(currentMonthIndex < 0) {
+            currentMonthIndex = 0;
+        }
     }
 
     /*
@@ -13,15 +43,15 @@ public class Calendar {
      */
     
     public String getTitle() {
-        return "";
+        return title;
     }
 
     public Month[] getMonths() {
-        return null;
+        return months;
     }
 
     public int getCurrentMonthIndex() {
-        return -1;
+        return currentMonthIndex;
     }
     
 }
--- src/test/model/CalendarTest.java
+++ src/test/model/CalendarTest.java
@@ -20,4 +20,37 @@ public class CalendarTest {
         assertEquals(12, calendar.getMonths().length);
         assertEquals(0, calendar.getCurrentMonthIndex());
     }
+
+    @Test
+    void testMonthIncrement() {
+        calendar.incrementMonthIndex();
+
+        assertEquals(1, calendar.getCurrentMonthIndex());
+    }
+
+    @Test
+    void testMonthIncrementUpper() {
+        for (int i = 0; i < 13; i++) {
+            calendar.incrementMonthIndex();
+        }
+
+        assertEquals(11, calendar.getCurrentMonthIndex());
+    }
+
+    @Test
+    void testMonthDecrement() {
+        calendar.incrementMonthIndex();
+        calendar.incrementMonthIndex();
+        calendar.decrementMonthIndex();
+
+        assertEquals(1, calendar.getCurrentMonthIndex());
+    }
+
+    @Test
+    void testMonthDecrementLower() {
+        calendar.decrementMonthIndex();
+
+        assertEquals(0, calendar.getCurrentMonthIndex());
+    }
+    
 }
