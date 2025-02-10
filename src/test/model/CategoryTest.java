7f26a3f9c8867deccf959ed7b7c2794cca253928
Jason Staker <jstaker@student.ubc.ca>
2025-02-09 17:33:38 -0800

    Added tests for Category

--- src/test/model/CategoryTest.java
+++ src/test/model/CategoryTest.java
@@ -1,19 +1,206 @@
 package model;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CategoryTest {
     
+    Category category1;
+    Category category2;
+    Category category3;
+
+    Subcategory subcategory1;
+    Subcategory subcategory2;
+
+    List<Subcategory> scList;
+
     @BeforeEach
     void setup() {
+        subcategory1 = new Subcategory();
+        subcategory2 = new Subcategory();
 
+        scList = new ArrayList<Subcategory>();
+        scList.add(subcategory1);
+        scList.add(subcategory2);
+
+        category1 = new Category();
+        category2 = new Category("test");
+        category3 = new Category("test", scList);
     }
 
     @Test
-    void testCategoryConstructor() {
-        assertTrue(true);
+    void testCategoryConstructorDefault() {
+        assertEquals("", category1.getName());
+        assertEquals(0, category1.getSubcategories().size());
+        assertEquals("", category1.getLocation());
+        assertEquals(0, category1.getLinks().size());
+        assertEquals(0, category1.getNotes().size());
     }
+
+    @Test
+    void testCategoryConstructorName() {
+        assertEquals("test", category2.getName());
+        assertEquals(0, category2.getSubcategories().size());
+        assertEquals("", category2.getLocation());
+        assertEquals(0, category2.getLinks().size());
+        assertEquals(0, category2.getNotes().size());
+    }
+
+    @Test
+    void testCategoryConstructorList() {
+        assertEquals("test", category3.getName());
+        assertEquals(scList, category3.getSubcategories());
+        assertEquals("", category3.getLocation());
+        assertEquals(0, category3.getLinks().size());
+        assertEquals(0, category3.getNotes().size());
+    }
+
+    @Test
+    void testAddSubcategory() {
+        category1.addSubcategory(subcategory1);
+
+        assertEquals(subcategory1, category1.getSubcategories().get(0));
+        assertEquals(1, category1.getSubcategories().size());
+    }
+
+    @Test
+    void testAddSubcategories() {
+        category1.addSubcategory(subcategory1);
+        category1.addSubcategory(subcategory2);
+
+        assertEquals(subcategory1, category1.getSubcategories().get(0));
+        assertEquals(subcategory2, category1.getSubcategories().get(1));
+        assertEquals(2, category1.getSubcategories().size());
+    }
+
+    @Test
+    void testRemoveSubcategory() {
+        category3.removeSubcategory(subcategory1);
+
+        assertEquals(subcategory2, category3.getSubcategories().get(0));
+        assertEquals(1, category3.getSubcategories().size());
+    }
+
+    @Test
+    void testRemoveSubcategories() {
+        category3.removeSubcategory(subcategory2);
+        category3.removeSubcategory(subcategory1);
+
+        assertEquals(0, category3.getSubcategories().size());
+    }
+
+    @Test
+    void testAddLink() {
+        category1.addLink("url1");
+
+        assertEquals("url1", category1.getLinks().get(0));
+        assertEquals(1, category1.getLinks().size());
+    }
+
+    @Test
+    void testAddLinks() {
+        category1.addLink("url1");
+        category1.addLink("url2");
+
+        assertEquals("url1", category1.getLinks().get(0));
+        assertEquals("url2", category1.getLinks().get(1));
+        assertEquals(2, category1.getLinks().size());
+    }
+
+    @Test
+    void testRemoveLinkString() {
+        category1.addLink("url1");
+        category1.removeLink("url1");
+
+        assertEquals(0, category1.getLinks().size());
+    }
+
+    @Test
+    void testRemoveLinkInt() {
+        category1.addLink("url1");
+        category1.removeLink(0);
+
+        assertEquals(0, category1.getLinks().size());
+    }
+
+    @Test
+    void testRemoveLinkStringDeep() {
+        category1.addLink("url1");
+        category1.addLink("url2");
+        category1.removeLink("url2");
+
+        assertEquals("url1", category1.getLinks().get(0));
+        assertEquals(1, category1.getLinks().size());
+    }
+
+    @Test
+    void testRemoveLinkIntDeep() {
+        category1.addLink("url1");
+        category1.addLink("url2");
+        category1.removeLink(1);
+
+        assertEquals("url1", category1.getLinks().get(0));
+        assertEquals(1, category1.getLinks().size());
+    }
+
+    @Test
+    void testAddNote() {
+        category1.addNote("note1");
+
+        assertEquals("note1", category1.getNotes().get(0));
+        assertEquals(1, category1.getNotes().size());
+    }
+
+    @Test
+    void testAddNotes() {
+        category1.addNote("note1");
+        category1.addNote("note2");
+
+        assertEquals("note1", category1.getNotes().get(0));
+        assertEquals("note2", category1.getNotes().get(1));
+        assertEquals(2, category1.getNotes().size());
+    }
+
+    @Test
+    void testRemoveNoteString() {
+        category1.addNote("note1");
+        category1.removeNote("note1");
+
+        assertEquals(0, category1.getNotes().size());
+    }
+
+    @Test
+    void testRemoveNoteInt() {
+        category1.addNote("note1");
+        category1.removeNote(0);
+
+        assertEquals(0, category1.getNotes().size());
+    }
+
+    @Test
+    void testRemoveNoteStringDeep() {
+        category1.addNote("note1");
+        category1.addNote("note2");
+        category1.removeNote("url2");
+
+        assertEquals("note1", category1.getNotes().get(0));
+        assertEquals(1, category1.getNotes().size());
+    }
+
+    @Test
+    void testRemoveNoteIntDeep() {
+        category1.addNote("note1");
+        category1.addNote("note2");
+        category1.removeNote(1);
+
+        assertEquals("note1", category1.getNotes().get(0));
+        assertEquals(1, category1.getNotes().size());
+    }
+
 }
