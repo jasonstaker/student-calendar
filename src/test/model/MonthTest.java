a213ac3be7f33a83d177a75012033e7bc363ea49
Jason Staker <jstaker@student.ubc.ca>
2025-02-10 18:03:44 -0800

    Added tests for Month

--- src/test/model/MonthTest.java
+++ src/test/model/MonthTest.java
@@ -1,19 +1,55 @@
 package model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MonthTest {
     
+    Month month1;
+    Month month2;
+    Month month3;
+
     @BeforeEach
     void setup() {
-
+        month1 = new Month(4);
+        month2 = new Month(5);
+        month3 = new Month(6);
     }
 
     @Test
     void testMonthConstructor() {
-        assertTrue(true);
+        assertEquals(4, month1.getMonthNumber());
+        assertEquals(new ArrayList<Day>(), month1.getDays());
+    }
+
+    @Test
+    void testHasDayPass() {
+        assertTrue(month1.hasDay(month1.getDays().get(0)));
+    }
+
+    @Test
+    void testHasDayFail() {
+        assertFalse(month1.hasDay(month2.getDays().get(0)));
+    }
+
+    @Test
+    void testIsBeforeBefore() {
+        assertTrue(month1.isBefore(month2));
+    }
+
+    @Test
+    void testIsBeforeEqual() {
+        assertFalse(month2.isBefore(month2));
+    }
+
+    @Test
+    void testIsBeforeAfter() {
+        assertFalse(month3.isBefore(month2));
     }
 }
