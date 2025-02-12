dc2851ef455c06b0a7421981471104efc4a45da7
Jason Staker <jstaker@student.ubc.ca>
2025-02-11 22:29:46 -0800

    Added specification for Time

--- src/main/model/Time.java
+++ src/main/model/Time.java
@@ -3,37 +3,28 @@ package model;
 // A Time with an hour and minute
 public class Time {
     
-    // REQUIRES: 
-    // MODIFIES: 
-    // EFFECTS: 
+    // REQUIRES: 0 <= hour <= 23, 0 <= minute <= 59
+    // EFFECTS: initializes a Time with the given hour and minute values
     public Time(int hour, int minute) {
 
     }
 
-    // REQUIRES: 
-    // MODIFIES: 
-    // EFFECTS: 
+    // EFFECTS: returns a string representation of the time
     public String timeToString() {
         return "";
     }
 
-    // REQUIRES: 
-    // MODIFIES: 
-    // EFFECTS: 
+    // EFFECTS: returns true if the given Time is equal to this Time, false otherwise
     public boolean equals(Time newTime) {
         return false;
     }
 
-    // REQUIRES: 
-    // MODIFIES: 
-    // EFFECTS: 
+    // EFFECTS: returns true if the given Time is before this Time, false otherwise
     public boolean isBefore(Time newTime) {
         return false;
     }
 
-    // REQUIRES: 
-    // MODIFIES: 
-    // EFFECTS: 
+    // EFFECTS: returns true if the given Time is after this Time, false otherwise
     public boolean isAfter(Time newTime) {
         return false;
     }
