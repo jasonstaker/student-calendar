531194e6f1eb4afc2d34ccc575c94fd48720a68c
Jason Staker <jstaker@student.ubc.ca>
2025-02-12 21:50:26 -0800

    Added tests for Year

---
+++ src/test/model/YearTest.java
@@ -0,0 +1,110 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class YearTest {
+
+    Year year1;
+    Year year2;
+
+    @BeforeEach
+    void setup() {
+        year1 = new Year(2025);
+        year2 = new Year(2024);
+    }
+
+    @Test
+    void testYearConstructor() {
+        assertEquals("January", year1.getCurrentMonth().getName());
+        assertEquals(12, year1.getMonths().size());
+        assertEquals(2025, year1.getYearNumber());
+    }
+
+    @Test
+    void testIncrementMonthIndex() {
+        year1.incrementMonthIndex();
+        assertEquals("February", year1.getCurrentMonth().getName());
+    }
+
+    @Test
+    void testIncrementMonthIndexOver() {
+        for(int i = 0; i < 18; i++) {
+            year1.incrementMonthIndex();
+        }
+        assertEquals("December", year1.getCurrentMonth().getName());
+    }
+
+    @Test
+    void testDecrementMonthIndex() {
+        year1.incrementMonthIndex();
+        year1.incrementMonthIndex();
+        year1.decrementMonthIndex();
+        assertEquals("February", year1.getCurrentMonth().getName());
+    }
+
+    @Test
+    void testDecrementMonthIndexUnder() {
+        for(int i = 0; i < 18; i++) {
+            year1.incrementMonthIndex();
+        }
+
+        for(int i = 0; i < 18; i++) {
+            year1.decrementMonthIndex();
+        }
+        assertEquals("January", year1.getCurrentMonth().getName());
+    }
+
+    @Test
+    void testIsBeforePass() {
+        assertTrue(year2.isBefore(year1));
+    }
+
+    @Test
+    void testIsBeforeFail() {
+        assertFalse(year2.isBefore(year2));
+    }
+
+    @Test
+    void testIsAfterPass() {
+        assertTrue(year1.isBefore(year2));
+    }
+
+    @Test
+    void testIsAfterFail() {
+        assertFalse(year2.isBefore(year2));
+    }
+
+    @Test
+    void testLeapYearCommonPass() {
+        Year leapYear = new Year(2024);
+        assertTrue(leapYear.isLeapYear());
+    }
+
+    @Test
+    void testLeapYearCommonFail() {
+        Year leapYear = new Year(2023);
+        assertFalse(leapYear.isLeapYear());
+    }
+
+    @Test
+    void testLeapYearDiv100() {
+        Year leapYear = new Year(1900);
+        assertFalse(leapYear.isLeapYear());
+    }
+
+    @Test
+    void testLeapYearDiv400() {
+        Year leapYear = new Year(2000);
+        assertTrue(leapYear.isLeapYear());
+    }
+    
+
+}
