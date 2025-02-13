57658decae2a090e42d7f498a9dab4cd8c7aadcb
Jason Staker <jstaker@student.ubc.ca>
2025-02-13 13:05:41 -0800

    Implemented Year

--- src/main/model/Year.java
+++ src/main/model/Year.java
@@ -6,31 +6,68 @@ import java.util.ArrayList;
 // A Year with a year number that contains a list of months and a current month/index
 public class Year {
 
+    private int yearNumber;
+    private List<Month> months;
+    private Month currentMonth;
+    private int currentMonthIndex;
+
     // initializes a Year with a given year number and 12 months
     public Year(int yearNumber) {
+        this.yearNumber = yearNumber;
+        currentMonthIndex = 0;
 
+        months = new ArrayList<Month>();
+    
+        for (int i = 0; i < 12; i++) {
+            months.add(new Month(this, i));
+        }
+
+        currentMonth = months.get(currentMonthIndex);
     }
 
     // MODIFIES: this
     // EFFECTS: increments the month index if it is < 11, nothing otherwise
     public void incrementMonthIndex() {
+        currentMonthIndex++;
 
+        if (currentMonthIndex > 11) {
+            currentMonthIndex = 11;
+        }
+
+        currentMonth = months.get(currentMonthIndex);
     }
 
     // MODIFIES: this
     // EFFECTS: decrements the month index if it is > 0, nothing otherwise
     public void decrementMonthIndex() {
+        currentMonthIndex--;
 
+        if (currentMonthIndex < 0) {
+            currentMonthIndex = 0;
+        }
+
+        currentMonth = months.get(currentMonthIndex);
     }
 
     // EFFECTS: returns true if this year is before the given year, false otherwise
     public boolean isBefore(Year year) {
-        return false;
+        return this.yearNumber < year.getYearNumber();
     }
 
     // EFFECTS: returns true if this year is after the given year, false otherwise
     public boolean isAfter(Year year) {
-        return false;
+        return this.yearNumber > year.getYearNumber();
+    }
+
+    // returns true if this year is a leap year, false otherwise
+    public boolean isLeapYear() {
+        if (this.yearNumber % 400 == 0) {
+            return true;
+        } else if (this.yearNumber % 100 == 0) {
+            return false;
+        }
+
+        return (this.yearNumber % 4 == 0);
     }
 
     /*
@@ -38,27 +75,27 @@ public class Year {
      */
 
     public int getYearNumber() {
-        return -1;
+        return yearNumber;
     }
 
     public List<Month> getMonths() {
-        return null;
+        return months;
     }
 
     public Month getCurrentMonth() {
-        return null;
+        return currentMonth;
     }
 
-    public void setYearNumber() {
-        
+    public void setYearNumber(int yearNumber) {
+        this.yearNumber = yearNumber;
     }
 
-    public void setMonths() {
-        
+    public void setMonths(List<Month> months) {
+        this.months = months;
     }
 
-    public void setCurrentMonth() {
-        
+    public void setCurrentMonth(Month currentMonth) {
+        this.currentMonth = currentMonth;
     }
     
 }
