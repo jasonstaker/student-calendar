0a92b682607d096f242b77d3803f1ec3f36d23a3
Jason Staker <jstaker@student.ubc.ca>
2025-02-13 15:09:00 -0800

    Added specification for MainMenuUI

---
+++ src/main/ui/MainMenuUI.java
@@ -0,0 +1,36 @@
+package ui;
+
+import model.Calendar;
+
+// the MainMenuUI handles main menu interactions
+public class MainMenuUI {
+
+    // EFFECTS: initializes the main menu with the given Calendar
+    public MainMenuUI(Calendar calendar) {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: starts the programs loop
+    public void start() {
+
+    }
+
+    // EFFECTS: displays the main menu
+    private void displayMainMenu() {
+
+    }
+
+    // REQUIRES: 1 <= choice <= 5
+    // MODIFIES: this
+    // EFFECTS: chooses the appropiate UI to enter based on the given choice
+    private boolean handleMenuChoice(int choice) {
+        return false;
+    }
+
+    // EFFECTS: prints out a basic space between UI's
+    private void makeWhiteSpace() {
+
+    }
+    
+}
