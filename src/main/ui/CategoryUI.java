17f74a94e099fb116adb2681abce12420fa9a528
Jason Staker <jstaker@student.ubc.ca>
2025-02-14 18:35:54 -0800

    Added Specification for CategoryUI

---
+++ src/main/ui/CategoryUI.java
@@ -0,0 +1,68 @@
+package ui;
+
+import model.Calendar;
+
+// A CategoryUI that handles the category menu interactions
+public class CategoryUI extends UI {
+
+    // EFFECTS: initializes this CategoryUI with the given calendar 
+    public CategoryUI(Calendar calendar) {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: starts the Category Manage loop which displays the Category Manage Menu
+    public void startCategoryMenu() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: starts the add category loop which displays the add category menu
+    public void startAddCategoryMenu() {
+        
+    }
+
+    // MODIFIES: this
+    // EFFECTS: starts the remove category loop which displays the remove category menu
+    public void startRemoveCategoryMenu() {
+        
+    }
+
+    // MODIFIES: this
+    // EFFECTS: starts the add subcategory loop which displays the add subcategory menu
+    public void startAddSubcategoryMenu() {
+        
+    }
+
+    // MODIFIES: this
+    // EFFECTS: starts the remove subcategory loop which displays the remove subcategory menu
+    public void startRemoveSubcategoryMenu() {
+        
+    }
+
+    // EFFECTS: displays the Manage Category menu
+    public void displayCategoryMenu() {
+
+    }
+
+    // EFFECTS: displays the Add Category menu
+    public void displayAddCategoryMenu() {
+
+    }
+
+    // EFFECTS: displays the Remove Category menu
+    public void displayRemoveCategoryMenu() {
+
+    }
+
+    // EFFECTS: displays the Add Subcategory menu
+    public void displayAddSubategoryMenu() {
+
+    }
+
+    // EFFECTS: displays the Remove Subcategory menu
+    public void displayRemoveSubategoryMenu() {
+
+    }
+
+}
