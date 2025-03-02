dec02fff0884dfc65abc4cededabb8ececb8af3d
Jason Staker <jstaker@student.ubc.ca>
2025-03-02 13:24:33 -0800

    Added tests for JsonReaderTest and JsonTest

---
+++ src/test/persistence/JsonReaderTest.java
@@ -0,0 +1,132 @@
+package persistence;
+
+import model.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JsonReaderTest extends JsonTest {
+
+    @Test
+    void testReaderNonExistentFile() {
+        JsonReader reader = new JsonReader("./data/noSuchFile.json");
+        try {
+            Calendar calendar = reader.read();
+            fail("IOException expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    void testReaderEmptyCalendar() {
+        JsonReader reader = new JsonReader("./data/testReaderEmptyCalendar.json");
+        try {
+            Calendar calendar = reader.read();
+            assertEquals("title", calendar.getTitle());
+            assertEquals(0, calendar.getCategories().size());
+            assertEquals(0, calendar.getSubcategories().size());
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
+    @Test
+    void testReaderEmptyYear() {
+        JsonReader reader = new JsonReader("./data/testReaderEmptyCalendar.json");
+        try {
+            Year year = reader.read().getYears().get(0);
+            checkYear(2024, year.getMonths(), year.getMonths().get(0), 0, year);
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
+    @Test
+    void testReaderEmptyMonth() {
+        JsonReader reader = new JsonReader("./data/testReaderEmptyCalendar.json");
+        try {
+            Month month = reader.read().getYears().get(0).getMonths().get(0);
+            checkMonth("January", month.getYear(), 0, month.getDays(), month);
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
+    @Test
+    void testReaderEmptyDay() {
+        JsonReader reader = new JsonReader("./data/testReaderEmptyCalendar.json");
+        try {
+            Day day = reader.read().getYears().get(0).getMonths().get(0).getDays().get(0);
+            checkDay(reader.read().getYears().get(0), reader.read().getYears().get(0).getCurrentMonth(), 1, day);
+            assertEquals(0, day.getEvents().size());
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
+    @Test
+    void testReaderGeneralCalendar() {
+        JsonReader reader = new JsonReader("./data/testReaderGeneralCalendar.json");
+        try {
+            Calendar calendar = reader.read();
+            assertEquals("title", calendar.getTitle());
+            List<String> links = new ArrayList<String>();
+            List<String> notes = new ArrayList<String>();
+            List<String> tags = new ArrayList<String>();
+            links.add("link");
+            notes.add("note");
+            tags.add("tag1");
+            tags.add("tag2");
+            checkCategory("category", "location", links, notes, calendar.getCategories().get(0));
+            checkSubcategory(null, 1, tags, calendar.getSubcategories().get(0));
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
+    @Test
+    void testReaderGeneralYear() {
+        JsonReader reader = new JsonReader("./data/testReaderGeneralCalendar.json");
+        try {
+            Year year = reader.read().getYears().get(0);
+            checkYear(2025, year.getMonths(), year.getMonths().get(0), 0, year);
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
+    @Test
+    void testReaderGeneralMonth() {
+        JsonReader reader = new JsonReader("./data/testReaderGeneralCalendar.json");
+        try {
+            Month month = reader.read().getYears().get(0).getMonths().get(0);
+            checkMonth("January", month.getYear(), 0, month.getDays(), month);
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
+    @Test
+    void testReaderGeneralDay() {
+        JsonReader reader = new JsonReader("./data/testReaderGeneralCalendar.json");
+        try {
+            Day day = reader.read().getYears().get(0).getMonths().get(0).getDays().get(0);
+            Event event = day.getEvents().get(0);
+            checkDay(reader.read().getYears().get(0), reader.read().getYears().get(0).getCurrentMonth(), 1, day);
+            checkEvent("event", event);
+            checkCategory("name", "location", new ArrayList<String>(), new ArrayList<String>(), event.getCategory());
+            checkSubcategory(null, 2, new ArrayList<String>(), event.getSubcategory());
+            checkTime(12, 44, event.getStartTime());
+            checkTime(13, 30, event.getEndTime());
+            checkDay(day.getYear(), day.getMonth(), 1, day.getEvents().get(0).getRecurringDays().get(0));
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
+}
---
+++ src/test/persistence/JsonTest.java
@@ -0,0 +1,73 @@
+package persistence;
+
+import model.*;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JsonTest {
+
+    protected void checkYear(int yearNumber, List<Month> months, Month currentMonth, int currentMonthIndex, Year year) {
+        assertEquals(yearNumber, year.getYearNumber());
+        assertEquals(currentMonth, year.getCurrentMonth());
+        assertEquals(currentMonthIndex, year.getCurrentMonth().getMonthNumber());
+
+        for (int i = 0; i < 12; i++) {
+            assertEquals(i, year.getMonths().get(i).getMonthNumber());
+        }
+    }
+
+    protected void checkMonth(String name, Year year, int monthNumber, List<Day> days, Month month) {
+        assertEquals(name, month.getName());
+        assertEquals(year, month.getYear());
+        assertEquals(monthNumber, month.getMonthNumber());
+        
+        for (int i = 1; i <= days.size(); i++) {
+            assertEquals(i, month.getDays().get(i - 1).getDayNumber());
+        }
+    }
+
+    protected void checkDay(Year year, Month month, int dayNumber, Day day) {
+        assertEquals(year, day.getYear());
+        assertEquals(year, day.getMonth());
+        assertEquals(year, day.getDayNumber());
+    }
+
+    protected void checkEvent(String name, Event event) {
+        assertEquals(name, event.getName());
+    }
+
+    protected void checkCategory(String name, String location, List<String> links, 
+                                    List<String> notes, Category category) {
+        assertEquals(name, category.getName());
+        assertEquals(location, category.getLocation());
+        assertEquals(links.size(), category.getLinks().size());
+        assertEquals(notes.size(), category.getNotes().size());
+
+        for (int i = 0; i < links.size(); i++) {
+            links.get(i).equals(category.getLinks().get(i));
+        }
+
+        for (int i = 0; i < notes.size(); i++) {
+            notes.get(i).equals(category.getNotes().get(i));
+        }
+    }
+
+    protected void checkSubcategory(Category parentCategory, int priorityLevel, 
+                                        List<String> tags, Subcategory subcategory) {
+        assertEquals(parentCategory, subcategory.getParentCategory());
+        assertEquals(priorityLevel, subcategory.getPriorityLevel());
+        assertEquals(tags.size(), subcategory.getTags().size());
+
+        for (int i = 0; i < tags.size(); i++) {
+            tags.get(i).equals(subcategory.getTags().get(i));
+        }
+    }
+
+    protected void checkTime(int hour, int minute, Time time) {
+        assertEquals(hour, time.getHour());
+        assertEquals(minute, time.getMinute());
+    }
+
+}
