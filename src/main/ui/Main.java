6e6f57c53c21a1e3d7f5ede256c1403205d37967
jstaker <jstaker@student.ubc.ca>
2025-03-20 19:46:59 -0700

    Reverted Main back to the normal state for the program

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -15,57 +15,16 @@ import java.time.LocalDate;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter calendar title: ");
-
-        String calendarTitle = scanner.nextLine();
-        Calendar calendar = new Calendar(calendarTitle, LocalDate.now().getYear());
+        Calendar calendar = new Calendar("My Calendar", LocalDate.now().getYear());
 
         System.out.println();
 
-        JsonWriter jsonWriter = new JsonWriter("./data/test.json");
-        JsonReader jsonReader = new JsonReader("./data/test.json");
-
-        List<String> notes = new ArrayList<String>();
-        notes.add("note");
-        List<String> links = new ArrayList<String>();
-        links.add("link");
-        List<String> tags = new ArrayList<String>();
-        tags.add("tag");
-        
-        Category category = new Category("Category", new ArrayList<Subcategory>(), calendarTitle, links, notes);
-        Subcategory subcategory1 = new Subcategory(category, 1, tags, "first", "", links, notes);
-        Subcategory subcategory2 = new Subcategory("second");
-        subcategory1.addSubcategory(subcategory2);
-        category.addSubcategory(subcategory1);
-        category.addSubcategory(subcategory2);
-        Day day = calendar.getYears().get(0).getMonths().get(0).getDays().get(0);
-        List<Day> days = new ArrayList<Day>();
-        days.add(day);
-        Event event = new Event(category, subcategory2, new Time(12, 0), new Time(13, 0), "event", days);
-        calendar.addCategory(category);
-        calendar.addSubcategory(subcategory1);
-        calendar.addSubcategory(subcategory2);
-        calendar.addEvent(event);
-
         try {
-            calendar.setIds();
-            jsonWriter.open();
-            jsonWriter.write(calendar);
-            jsonWriter.close();
-            System.out.println("Saved " + calendar.getTitle() + " to ./data/test.json");
-        } catch (FileNotFoundException e) {
-            System.out.println("Unable to write to file: ./data/test.json");
+        new CalendarUI(calendar);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
         }
-
-        try {
-            calendar = jsonReader.read();
-            System.out.println("Loaded " + calendar.getTitle() + " from ./data/test.json");
-        } catch (IOException e) {
-            System.out.println("Unable to read from file: ./data/test.json");
-        }
-
-        scanner.close();
     }
 
 }
